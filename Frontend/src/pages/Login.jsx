3cf690bfe9a626b240fbd51de267a1e9523aaf1f
Ajay singh <121940011+AJ-s03@users.noreply.github.com>
2024-08-01 16:20:25 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
         const username = document.getElementById("usernameauth").value;
         const password = document.getElementById("passwordauth").value;
         e.preventDefault();
-        axios.post("http://localhost:3000/api/Login", { username, password }, {withCredentials: true})
+        axios.post("https://regulaexpi-backend.onrender.com/api/Login", { username, password }, {withCredentials: true})
             .then(result => {
                 if(result.data.session)
                     navigate("/Home");
